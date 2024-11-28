4d3d77797284a4cac22d7d1374bc339978a3ccea
Rúben Alves <rubenanapu@hotmail.com>
2024-11-28 15:52:58 -0300

    Loading built vue static files with the ?v=build-datetime on the generated index.html file to make it clearer and free of cache-related problems. Also, created some folders with a __git__ file just to make sure the folder exists on git

--- django_vue_generator/ui/vite.config.js
+++ django_vue_generator/ui/vite.config.js
@@ -3,16 +3,49 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dayjs from 'dayjs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // ----------------------------------------------------------------------------
+    // Modifying the content the generated index.html to append something like
+    //  ?v=2024.28.11-14.17.53 to the static assets to avoid caching-related issues
+    // ----------------------------------------------------------------------------
+    {
+      name: 'html-transform',
+      apply: 'build',
+      transformIndexHtml(html) {
+        const version = dayjs().format('YYYY.MM.DD-HH.mm.ss');
+
+        // Finds all <script> tags with a src attribute and appends ?v=timestamp to the URL.
+        return html.replace(/<script.*?src="(.*?)".*?>/g, (match, src) => {
+          return match.replace(src, `${src}?v=${version}`);
+
+          // Finds all <link> tags (e.g., for CSS) and appends the same ?v=timestamp.
+        }).replace(/<link.*?href="(.*?)".*?>/g, (match, href) => {
+          return match.replace(href, `${href}?v=${version}`);
+        });
+      },
+    },
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  // ------------------------------------------------------
+  // Rule for not adding strange suffix for generated files
+  // ------------------------------------------------------
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  }
 })
